5795221ad031c1f141c62013eb62b41a5fb14752
Puiu Petre Daniel <petredaniel.puiu@yahoo.ro>
2026-02-08 21:47:10 +0200

    - change led pin to 12 as 13 blinks in the beginning - changed sensor pin to 2 as 1 is serial + added logging

--- Components/Sensors/KY-003_Hall_Magnet/Example/Example.ino
+++ Components/Sensors/KY-003_Hall_Magnet/Example/Example.ino
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
-static const uint8_t SENSOR_PIN = 1;
-static const uint8_t LED_PIN = 13;
+static const uint8_t SENSOR_PIN = 2;
+static const uint8_t LED_PIN = 12;
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(SENSOR_PIN, INPUT_PULLUP);
   //pinMode(SENSOR_PIN, INPUT); // use this if you are using an external PULL-UP to VCC with a 10 kOhm resistor
 
@@ -13,4 +15,7 @@ void setup() {
 void loop() {
   int sensorValue = digitalRead(SENSOR_PIN);
   digitalWrite(LED_PIN, !sensorValue);
+  
+  Serial.print("Sensor value: ");
+  Serial.println(sensorValue);
 }
